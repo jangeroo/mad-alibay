de58e9447868c6d7d424c233a06a521165aa2276
Michael Jang <michaeljang86@gmail.com>
2017-11-10 14:02:53 -0500

    added some item listings

--- src/TestDataGenerator.js
+++ src/TestDataGenerator.js
@@ -8,7 +8,20 @@ class TestDataGenerator extends Component {
         // this.generateData()
     }
     generateData() {
-        backend.createListing('Mickey Mouse', 12.99, "Mickey ears")
+        // SET UP SOME LISTINGS FOR DIFFERENT VENDORS
+        backend.createListing(123,15,'Green polo shirt')
+        backend.createListing(123,135, 'Blue Tux')
+        backend.createListing(123,76, 'AlbertoTime Jeans')
+
+        backend.createListing(345,49.99, 'Ergonomic wireless gaming mouse')
+        backend.createListing(345,79.99, 'Ergonomic wireless gaming keyboard')
+        backend.createListing(345, 'Samsung 1TB Hard drive')
+
+        backend.createListing(567,3.99, 'Long stem rose')
+        backend.createListing(567,29.99, '1 dozen long stem roses')
+
+        // MAKE SOME PURCHASES
+        backend.buy()
     }
 
     render() {

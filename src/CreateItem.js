cd2632503ef8f31d51f74472d45e62c32501227f
Michael Jang <michaeljang86@gmail.com>
2017-11-12 23:37:49 -0500

    create item provides feedback to seller

--- src/CreateItem.js
+++ src/CreateItem.js
@@ -4,15 +4,31 @@ import backend from './backend/firebase-backend.js';
 
 class CreateItem extends Component {
 
+    constructor() {
+        super();
+        this.state = { createdListing: null }
+    }
+
     _handleFormSubmit = async event => {
         event.preventDefault();
         let image = 'https://www.thepaperworker.com/media/extendware/ewimageopt/media/inline/85/8/generic-product-54d.jpg'
-        await backend.createListing(this.props.sellerID, this.inputPrice.value, image, this.inputBlurb.value)
+        let listintgID = await backend.createListing(this.props.sellerID, this.inputPrice.value, image, this.inputBlurb.value)
         this.inputPrice.value = ''
         this.inputBlurb.value = ''
+        this.setState({createdListing: listintgID})
+    }
+
+    _createAnotherItem = event => {
+        this.setState({createdListing: null})
     }
 
     render() {
+        if (this.state.createdListing) return (
+            <div>
+                <h2>Item created successfully.</h2>
+                <button onClick={this._createAnotherItem} className="btn">Create another item</button>
+            </div>
+        )
         return (
             <div className="CreateItem">
                 <form onSubmit={this._handleFormSubmit}>

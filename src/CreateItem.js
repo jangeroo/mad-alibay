30457f55ea0aec7283d7782142f51f0dcf7d6940
Michael Jang <michaeljang86@gmail.com>
2017-11-12 23:25:24 -0500

    create item is working

--- src/CreateItem.js
+++ src/CreateItem.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import backend from './backend/firebase-backend.js';
 
 
 class CreateItem extends Component {
 
+    _handleFormSubmit = async event => {
+        event.preventDefault();
+        let image = 'https://www.thepaperworker.com/media/extendware/ewimageopt/media/inline/85/8/generic-product-54d.jpg'
+        await backend.createListing(this.props.sellerID, this.inputPrice.value, image, this.inputBlurb.value)
+        this.inputPrice.value = ''
+        this.inputBlurb.value = ''
+    }
+
     render() {
         return (
             <div className="CreateItem">
-                <form>
+                <form onSubmit={this._handleFormSubmit}>
                     <div>
-                        <input placeholder="Blurb" />
-                        <input placeholder="Price" />
+                        <input ref={r => this.inputBlurb = r} placeholder="Blurb" />
+                        <input ref={r => this.inputPrice = r} placeholder="Price" />
                     </div>
                     <button className="btn">Create</button>
                 </form>

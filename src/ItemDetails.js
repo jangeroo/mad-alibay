75c3f17e490f203fef93cb78b2f913925cf5c3ef
Diana Chiu <orangesmartie92@gmail.com>
2017-11-12 17:03:19 -0800

    adding itemsdetail link

--- src/ItemDetails.js
+++ src/ItemDetails.js
@@ -1,42 +1,38 @@
 import React, { Component } from 'react';
-import { getItemDescription, allListings } from './main';
-
-const items = [getItemDescription(allListings())];
-
-// const listings = allListings();
-// const itemDescription = getItemDescription(listings);
-// const items = [itemDescription];
-
-console.log(items);
+import backend from './backend/firebase-backend.js';
+import Catelogue from './Catelogue.js';
 
 class ItemDetails extends Component {
-    
-        constructor(props) {
-            super(props);
-    
-            this.state = {
-                items
-            }
-    
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            item: { productID: "", price: 0, blurb: "", image: "" }
         }
-    
-        render() {
-            return (
-                <div className="container">
-                    <hr/>
-                    <h4> Total item count: <span className="badge">{this.state.items.length}</span></h4>
-                    <ul className="list-group">
-                            {this.state.items.map((item, index) =>
-                            <li className="list-group-item" key={index}>
-                            <h4 className="list-group-item-heading"> {item.blurb} </h4>
-                            <p>$ {item.price} </p>
-                            {/* <button className="btn btn-danger btn-sm" onClick={this.removeItem.bind(this, index)}><span className="glyphicon glyphicon-trash"></span> Delete</button> */}
-                            </li>
-                        )}
-                    </ul>
-                </div>
-            );
-        }
+
     }
-    
-    export default ItemDetails;
+
+    componentWillMount() {
+        backend.getItemDescription(this.props.productID)
+            .then(
+            item => {
+                this.setState({
+                    item: { productID: this.props.productID, price: item.price, blurb: item.blurb, image: item.image }
+                });
+            }
+            )
+
+    }
+
+    render() {
+        return (
+            <div className="catelogue-container" key={this.state.item.blurb}>
+                <Catelogue item={this.state.item} />
+            </div>
+
+        );
+    }
+}
+
+export default ItemDetails;
